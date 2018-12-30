e9c34af7036fe5d6a84e2e89416ee3b3ac0ae78e
Peter <jps2000@users.noreply.github.com>
2018-12-30 16:08:53 +0100

    Update Arduino_BNO080_2.ino
    Added nrf52 wire handling
    Improved quat calculation. Depending on compiler sometimes int to float conversion was misinterpreted--> QP need to be applied to int16_t
    Added Euler angle calculation in utilities

--- Arduino_BNO080_2.ino
+++ Arduino_BNO080_2.ino
@@ -39,7 +39,8 @@
 /*************************************
                 DEFINITIONS
  *************************************/
-//#define Simblee
+//#define nrf52
+//#define Simblee       
 #define Nano
 
 #define Led                       10              // Led  (Arduino nano = 13) to measure loop duration (10)
@@ -52,11 +53,9 @@ int plot_interval                 = 1000;          // plot interval in ms
 uint8_t cargo[23]; 
 uint8_t next_data_seqNum          = 0x10;         // next data sequence number 
 
-float q0,q1,q2,q3;                                // quaternions q0 = qw 1 = i; 2 = j; 3 = k;
-float h_est;                                      // heading accurracy estimation
-uint8_t stat_;                                     // Status (0-3)
-
-                               
+int16_t q0,q1,q2,q3,h_est;                        // quaternions q0 = qw 1 = i; 2 = j; 3 = k;  h_est = heading accurracy estimation
+float Q0,Q1,Q2,Q3,H_est;                                     
+uint8_t stat_;                                    // Status (0-3)
 
 const uint8_t quat_report        = 0x05;          // defines kind of rotation vector (0x05), geomagnetic (0x09), AR/VR (0x28),                                                 // without magnetometer : game rotation vector (0x08), AR/VR Game (0x29)
 const int reporting_frequency    = 200;            // reporting frequency in Hz  // note that serial output strongly reduces data rate
@@ -90,7 +89,12 @@ void setup() {
   
   #ifdef Simblee 
   Wire.speed = 400;  // select 400 kbps (100, 250)   // 
-  Wire.beginOnPins(5,6);               // (SCLpin,SDApin)
+  Wire.beginOnPins(5,6);                 // (SCLpin,SDApin)
+  #endif
+ 
+  #ifdef nrf52
+  Wire.begin();                          // no need to define pins. they are declared in variants.h
+  Wire.setClock(400000L);
   #endif
  
   #ifdef Nano
@@ -99,7 +103,7 @@ void setup() {
   #endif
   
   Serial.println ("*********************************************************************");
-  Serial.println (" Rock bottom code for  BNO080 on Atmega 328p; Cortex M0 (Simblee) V1.0 2018-01-22");
+  Serial.println (" Rock bottom code for  BNO080 on Atmega 328p; Cortex M0 (Simblee) and M4 (nrf52)  V1.1 2018-12-30");
   Serial.println ("*********************************************************************");
 
 // BNO settings
@@ -123,16 +127,16 @@ void loop() {
   
  if (millis() - plot_interval > 0){ 
         Serial.print ("S "); Serial.print (stat_);
-        Serial.print ("; E "); Serial.print (h_est + 0.05f,1);                   // including rounding
-        Serial.print ("; q0 "); Serial.print (q0 + 0.00005f,4);                  // = qw (more digits to find out north direction (y axis N --> q0 = 1)
-        Serial.print ("; q1 "); Serial.print (q1 + 0.0005f,3);
-        Serial.print ("; q2 "); Serial.print (q2 + 0.0005f,3);
-        Serial.print ("; q3 "); Serial.println (q3 + 0.0005f,3);
+        Serial.print ("; E "); Serial.print (H_est + 0.05f,1);                   // including rounding
+        Serial.print ("; q0 "); Serial.print (Q0 + 0.00005f,4);                  // = qw (more digits to find out north direction (y axis N --> q0 = 1)
+        Serial.print ("; q1 "); Serial.print (Q1 + 0.0005f,3);
+        Serial.print ("; q2 "); Serial.print (Q2 + 0.0005f,3);
+        Serial.print ("; q3 "); Serial.println (Q3 + 0.0005f,3);
         plot_interval = millis();
  }
  
-                           
-  if (stat_ == 3)  digitalWrite(Led, !digitalRead(Led));                   // blink Led every loop run (--> to measure loop duration);             
+  //if (stat_ == 3)  digitalWrite(Led,HIGH);                              // indicate cal status 3
+  if (stat_ == 3)  digitalWrite(Led, !digitalRead(Led));                  // blink Led every loop run (--> to measure loop duration);             
   else digitalWrite(Led, LOW);                                            // status Led 
 
 
@@ -187,21 +191,22 @@ void get_QUAT(){
       i++;
       }
   }
-      if((cargo[9] == quat_report) && ((cargo[10]) == next_data_seqNum )){      // check for report and incrementing data seqNum
+      if((cargo[9] == quat_report) {                                            // check for report 
+      //if((cargo[9] == quat_report) && ((cargo[10]) == next_data_seqNum )){    // check for report and incrementing data seqNum
         next_data_seqNum = ++cargo[10];                                         // predict next data seqNum              
-        stat_ = cargo[11] & 0x03;                                                // bits 1:0 contain the status (0,1,2,3)  
+        stat_ = cargo[11] & 0x03;                                               // bits 1:0 contain the status (0,1,2,3)  
     
         q1 = (((int16_t)cargo[14] << 8) | cargo[13] ); 
         q2 = (((int16_t)cargo[16] << 8) | cargo[15] );
         q3 = (((int16_t)cargo[18] << 8) | cargo[17] );
         q0 = (((int16_t)cargo[20] << 8) | cargo[19] ); 
 
-        q0 *= QP(14); q1 *= QP(14); q2 *= QP(14); q3 *= QP(14);                  // apply Q point (quats are already unity vector)
+        Q0 = q1 * QP(14); Q1 = q1 * QP(14); Q2 = q2 * QP(14); Q3 = q3 * QP(14);  // apply Q point (quats are already unity vector)
 
        if (quat_report == 0x05 || quat_report == 0x09 || quat_report == 0x28 ){  // heading accurracy only in some reports available
         h_est = (((int16_t)cargo[22] << 8) | cargo[21] );                        // heading accurracy estimation  
-        h_est *= QP(12);                                                         // apply Q point 
-        h_est *= radtodeg;                                                       // convert to degrees                
+        H_est = h_est * QP(12);                                                  // apply Q point this is 12  not 14
+        H_est *= radtodeg;                                                       // convert to degrees                
        }
        
       
@@ -298,5 +303,20 @@ void ME_cal(uint8_t P0, uint8_t P1, uint8_t P2, uint8_t P4){
  R20 = q2 * q4 - q1 * q3;
  R21 = q3 * q4 + q1 * q2;
  R22 = q1 * q1 + q4 * q4 -0.5f; // = 0.5f -q2 * q2 -q3 * q3;
+ 
+ 
+ # Euler angles
+        yaw   =  atan2f(Q1 * Q2 + Q0 * Q3, Q0 * Q0 + Q1 * Q1 - 0.5f);   // heading
+        pitch =  -  asinf(2.0f * (Q1 * Q3 - Q0 * Q2));
+        roll  =  atan2f(Q0 * Q1 + Q2 * Q3, Q0 * Q0 + Q3 * Q3 - 0.5f);
+  
+        yaw += PI * 0.5f;                                               // correction of the y axis direction if needed
+        if(yaw < 0) yaw += 2.0f * PI;
+        if(yaw > 2.0f *PI) yaw -= 2.0f * PI;  
+  
+        yaw   *= radtodeg;
+        yaw    = 360.0f - yaw;                         // correction of the y axis direction if needed; apply location offset also here
+        pitch *= radtodeg;
+        roll  *= radtodeg;
 
 */
