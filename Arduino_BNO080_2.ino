178bc664b3e71c0e253cc30d940043e9174ba43f
Peter <jps2000@users.noreply.github.com>
2019-05-03 18:04:04 +0200

    Update Arduino_BNO080_2.ino
    Changed get_QUAT
    patch for wrong  quats from BNO080 of unknown origin. checks if received quats are unity vector.

--- Arduino_BNO080_2.ino
+++ Arduino_BNO080_2.ino
@@ -53,7 +53,7 @@ int plot_interval                 = 1000;         // plot interval in ms
 uint8_t cargo[23]; 
 uint8_t next_data_seqNum          = 0x10;         // next data sequence number 
 
-int16_t q0,q1,q2,q3,h_est;                        // quaternions q0 = qw 1 = i; 2 = j; 3 = k;  h_est = heading accurracy estimation
+
 float Q0,Q1,Q2,Q3,H_est;                                     
 uint8_t stat_;                                    // Status (0-3)
 
@@ -174,8 +174,11 @@ void loop() {
  */
 
 
-void get_QUAT(){                                                               
-  if (quat_report == 0x08 || quat_report == 0x29){
+void get_QUAT(){ 
+ int16_t q0,q1,q2,q3,h_est;                        // quaternions q0 = qw 1 = i; 2 = j; 3 = k;  h_est = heading accurracy estimation
+ float a,b,c,d,norm;
+ 
+ if (quat_report == 0x08 || quat_report == 0x29){
     Wire.requestFrom(BNO_ADDRESS,21);
     int i=0; 
     while (Wire.available()){
@@ -202,7 +205,12 @@ void get_QUAT(){
         q3 = (((int16_t)cargo[18] << 8) | cargo[17] );
         q0 = (((int16_t)cargo[20] << 8) | cargo[19] ); 
 
-        Q0 = q1 * QP(14); Q1 = q1 * QP(14); Q2 = q2 * QP(14); Q3 = q3 * QP(14);  // apply Q point (quats are already unity vector)
+     // patch for  rarely occurring wrong data from BNO of unknown reasons. --> Check if q(0,1,2,3) is not a unity vector
+        a = q0 * QP(14); b = q1 * QP(14); c = q2 * QP(14); d = q3 * QP(14);    // apply Q point (quats are already unity vector)
+        norm = sqrtf(a * a + b * b + c * c + d * d);
+        if(abs(norm - 1.0f) > 0.0015) return;                                   // skip faulty quats
+        
+        Q0 = a; Q1 = b; Q2 = c; Q3 = d;
 
        if (quat_report == 0x05 || quat_report == 0x09 || quat_report == 0x28 ){  // heading accurracy only in some reports available
         h_est = (((int16_t)cargo[22] << 8) | cargo[21] );                        // heading accurracy estimation  
