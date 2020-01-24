a7667f4aeac4bebd3d3ea4497fcdf8ab5fbaa28c
Peter <jps2000@users.noreply.github.com>
2020-01-24 12:41:37 +0100

    Update Arduino_BNO080_2.ino

--- Arduino_BNO080_2.ino
+++ Arduino_BNO080_2.ino
@@ -403,16 +403,27 @@ void get_Product_ID(){
  To rotate a vector V = (vx,vx,vz) in quaternion frame
  
  W = (wx,wy,wz) 
- wx = R00 * vx + R10 * vy + R20 * vz;
- wy = R01 * vx + R11 * vy + R21 * vz:
- wz = R02 * vx + R12 * vy + R22 * vz:
- 
+ wx = 2 * ( R00 * vx + R10 * vy + R20 * vz);
+ wy = 2 * ( R01 * vx + R11 * vy + R21 * vz):
+ wz = 2 * ( R02 * vx + R12 * vy + R22 * vz):
+
  For example gravity vector  g = (0,0,1)  --> G = (gx,gy,gz) = (R20, R21, R22)
+
+ Euler angles from rotation matrix:
  
- # Euler angles
+ yaw   =  atan2f(R10 , R00);                    // heading
+ pitch =  -  asinf(R20); =  -  asinf(gx);
+ roll  =  atan2f(R21 , R22); = atan2f(gy , gz);
+ 
+ 
+ 
+ 
+ # Euler angles from rotation matrix
  yaw   =  atan2f(Q1 * Q2 + Q0 * Q3, Q0 * Q0 + Q1 * Q1 - 0.5f);   // heading
  pitch =  -  asinf(2.0f * (Q1 * Q3 - Q0 * Q2));
  roll  =  atan2f(Q0 * Q1 + Q2 * Q3, Q0 * Q0 + Q3 * Q3 - 0.5f);
+ 
+
   
  yaw += PI * 0.5f;                                               // correction of the y axis direction if needed
  if(yaw < 0) yaw += 2.0f * PI;
